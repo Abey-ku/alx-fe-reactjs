258e495d26466d0b2eb1bb9937d3bd101b0e5afb
Albert Sam <aaasamuelsss@gmail.com>
2024-08-18 19:14:46 +0100

    update App.jsx file

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar'; // Import the Navbar component
+import Home from './Components/Home';
+import About from './Components/About';
+import Services from './Components/Services';
+import Contact from './Components/Contact';
+import Navbar from './Components/Navbar'; // Import the Navbar component
 
 function App() {
   return (
