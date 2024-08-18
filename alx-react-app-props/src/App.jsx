848d86c4476d2e5542f48fe021dcdb0ead34c5a5
Albert Sam <aaasamuelsss@gmail.com>
2024-08-18 17:24:47 +0100

    update files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import UserContext from './UserContext'; // Import UserContext
 
 function App() {
+  // Define the user data that you want to share across components
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
+    // Wrap the ProfilePage component inside UserContext.Provider
     <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,6 +1,8 @@
-import UserContext from '../UserContext';
+import React, { useContext } from 'react';
+import UserContext from '../UserContext'; // Import UserContext
 
 function UserDetails() {
+  // Use the useContext hook to consume the UserContext
   const userData = useContext(UserContext);
 
   return (
