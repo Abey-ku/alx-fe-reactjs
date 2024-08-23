da3b270d8ae0b852f0079ce7d773756db7aa6f7b
Albert Sam <aaasamuelsss@gmail.com>
2024-08-23 01:31:44 +0100

    update App.jsx file

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
   return (
     <Router>
       <div>
-        <Navbar /> 
         <Routes>
+          <Route path="/Navbar" element={<Navbar />} />
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
